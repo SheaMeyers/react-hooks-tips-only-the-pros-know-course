66419fb236a8b1789e36304079fd07c5aaf6e5da
Shea Meyers <shea.meyers@gmail.com>
2021-08-08 13:37:12 +0200

    Add useDebugValue hook

--- src/hooks/useThrottle.ts
+++ src/hooks/useThrottle.ts
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react";
+import { useDebugValue, useEffect, useRef } from "react";
 
 export function useThrottle(fn: () => void, timeout: number): void {
     const previousRef = useRef<(() => void) | null>(null)
@@ -6,6 +6,8 @@ export function useThrottle(fn: () => void, timeout: number): void {
     if (previousRef.current !== fn) {
         currentRef.current = fn
     }
+
+    useDebugValue(currentRef.current, (fn) => fn?.toString())
 
     useEffect(() => {
         const handle = setInterval(() => {
